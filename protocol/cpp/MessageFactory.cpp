53b64e07b4850e964db523a9191fa1f3885dc7bc
sjlx1020 <APPLE@kkfunmatoMacBook-Pro.local>
2013-08-04 15:01:24 +0800

    Revert "change msg from thrift format to protobuf"
    This reverts commit 59151b683b7b66e9a5a984b1804e11182b38efb4.

---
+++ protocol/cpp/MessageFactory.cpp
@@ -0,0 +1,37 @@
+#include "MessageFactory.h"
+#include "sb.LoginRequest.pb.h"
+#include "sb.LoginResponse.pb.h"
+using namespace org::net::obj;
+static MessageFactory* factory=NULL;
+MessageFactory* MessageFactory::shareMessageFactory()
+{
+ 	if(factory==NULL){
+ 		factory=new MessageFactory();
+ 	}
+	return factory;
+}
+
+void MessageFactory::parseMsg(ByteBuffer* buf)
+{
+	int len=buf->readInt();
+	int msg_type=buf->readInt();
+	int body_len=len-8;
+	::google::protobuf::Message *msg_obj=NULL;
+	switch (msg_type)
+	{
+		case LOGINREQUEST_TYPE:
+		{
+			msg_obj=LoginRequest::default_instance().New();
+			msg_obj->ParseFromArray(buf->buffer, body_len);
+			break;
+		}
+		case LOGINRESPONSE_TYPE:
+		{
+			msg_obj=LoginRequest::default_instance().New();
+			msg_obj->ParseFromArray(buf->buffer, body_len);
+			break;
+		}
+		default:break;
+	}
+	GameService::shareService()->disPatchMsg(msg_type, msg_obj);
+}
